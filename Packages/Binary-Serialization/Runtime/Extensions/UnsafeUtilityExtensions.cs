391767f3394db13d532eb631f09cd47190fb48c9
Gr871 <ky3971@yandex.ru>
2023-01-19 00:17:52 +0300

    .

--- Packages/Binary-Serialization/Runtime/Extensions/UnsafeUtilityExtensions.cs
+++ Packages/Binary-Serialization/Runtime/Extensions/UnsafeUtilityExtensions.cs
@@ -3,23 +3,57 @@
 namespace BinarySerialization.Extensions {
     public static class UnsafeUtilityExtensions {
         
+        public static unsafe void* OffsetPtr(IntPtr ptr, int offset, int sizeOfType) {
+            return IntPtr.Add(ptr, offset * sizeOfType).ToPointer();
+        }
         public static unsafe void* OffsetPtr<T>(IntPtr ptr, int offset) where T : unmanaged {
-            return IntPtr.Add(ptr, offset * sizeof(T)).ToPointer();
+            return OffsetPtr(ptr, offset, sizeof(T));
+        }
+        
+        //
+
+        public static unsafe void* OffsetPtr(void* ptr, int offset, int sizeOfType) {
+            return OffsetPtr((IntPtr)ptr, offset, sizeOfType); 
         }
         
         public static unsafe void* OffsetPtr<T>(void* ptr, int offset) where T : unmanaged {
-            return OffsetPtr<T>((IntPtr)ptr, offset); 
+            return OffsetPtr(ptr, offset, sizeof(T)); 
+        }
+        
+        //
+        
+        public static IntPtr OffsetIntPtr(IntPtr ptr, int offset, int sizeOfType) {
+            return IntPtr.Add(ptr, offset * sizeOfType);
         }
         
         public static unsafe IntPtr OffsetIntPtr<T>(IntPtr ptr, int offset) where T : unmanaged {
-            return IntPtr.Add(ptr, offset * sizeof(T));
+            return OffsetIntPtr(ptr, offset, sizeof(T));
         }
+        
+        //
+        
+        public static unsafe IntPtr OffsetIntPtr(void* ptr, int offset, int sizeOfType) {
+            return OffsetIntPtr((IntPtr)ptr, offset, sizeOfType); 
+        }
+        
         public static unsafe IntPtr OffsetIntPtr<T>(void* ptr, int offset) where T : unmanaged {
             return OffsetIntPtr<T>((IntPtr)ptr, offset); 
         }
         
-        public static unsafe ref T OffsetRef<T>(IntPtr ptr, int offset) where T : unmanaged {
-            return ref *(T*)(ptr + offset * sizeof(T));
+        //
+        
+        public static unsafe ref T Ref<T>(IntPtr ptr) where T : unmanaged {
+            return ref *(T*)ptr;
         }
+        
+        public static unsafe ref T OffsetRef<T>(IntPtr ptr, int offset, int sizeOfType) where T : unmanaged {
+            return ref Ref<T>(ptr + offset * sizeOfType);
+        }
+        
+        public static unsafe ref T OffsetRef<T>(IntPtr ptr, int offset) where T : unmanaged {
+            return ref OffsetRef<T>(ptr, offset, sizeof(T));
+        }
+        
+        
     }
 }
